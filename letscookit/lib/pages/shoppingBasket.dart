9903991f47596c4bbf9f822f251052fb3aa48e1e
elB1MBO <raulduran.lacimada@gmail.com>
2022-04-13 19:25:42 +0200

    shopping basket in process

--- letscookit/lib/pages/shoppingBasket.dart
+++ letscookit/lib/pages/shoppingBasket.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:letscookit/utilities/ingrediente.dart';
+
+import '../utilities/lista_Ingredientes.dart';
+import '../utilities/receta.dart';
 
 class shoppingBasket extends StatefulWidget {
   shoppingBasket({Key? key}) : super(key: key);
@@ -8,10 +12,58 @@ class shoppingBasket extends StatefulWidget {
 }
 
 class _shoppingBasketState extends State<shoppingBasket> {
+  bool isChecked = false;
+  int i = 0;
+
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Shopping basket'),
+    Receta receta = Receta("Pollo al limón", 2);
+    ListaIngredientes l1 = ListaIngredientes();
+    var map = {"ingrediente": "pollo", "check": "false"};
+    l1.add(map);
+
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Color.fromARGB(255, 52, 160, 164);
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            for (i = 0; i<l1.length(); i++) ...[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(l1.get(i).toString()),
+                  Checkbox(
+                    checkColor: Colors.white,
+                    fillColor: MaterialStateProperty.resolveWith(getColor),
+                    value: l1.get(i)["check"], //Aqui habria que establecer a value al bool del map
+                    onChanged: (bool? value) {
+                      setState(() {
+                        l1.get(i)["check"] = value!;
+                      });
+                    },
+                  ),
+                ],
+              )
+            ]
+          ],
+        )
+      ],
     );
   }
 }
