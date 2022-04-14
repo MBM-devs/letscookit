31ba56be4a4bbf0212e768c9c80b3a7a46296fe2
elB1MBO <raulduran.lacimada@gmail.com>
2022-04-14 20:39:23 +0200

    falta checkbox correcta

--- letscookit/lib/pages/shoppingBasket.dart
+++ letscookit/lib/pages/shoppingBasket.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:letscookit/utilities/ingrediente.dart';
 
 import '../utilities/lista_Ingredientes.dart';
+import '../utilities/lista_compra.dart';
 import '../utilities/receta.dart';
 
 class shoppingBasket extends StatefulWidget {
@@ -13,17 +14,25 @@ class shoppingBasket extends StatefulWidget {
 
 class _shoppingBasketState extends State<shoppingBasket> {
   bool isChecked = false;
-  int i = 0;
-
 
   @override
   Widget build(BuildContext context) {
-    Receta receta = Receta("Pollo al limón", 2);
-    ListaIngredientes l1 = ListaIngredientes();
-    var map = {"ingrediente": "pollo", "check": "false"};
-    l1.add(map);
+    Ingrediente pollo = new Ingrediente("Pollo");
+    Ingrediente pan = new Ingrediente("Pan");
+    Ingrediente ajo = new Ingrediente("Ajo");
+    Map<Ingrediente, bool> mapaCompra = {
+      pollo : false,
+      pan : true,
+      ajo : false
+    };
+    //Lista de maps
+    List<Map<Ingrediente, bool>> listaCompra = [
+      {pollo : false},
+      {pan : true}
+    ];
+    int _cont = 0;
 
-    Color getColor(Set<MaterialState> states) {
+    /*Color getColor(Set<MaterialState> states) {
       const Set<MaterialState> interactiveStates = <MaterialState>{
         MaterialState.pressed,
         MaterialState.hovered,
@@ -33,37 +42,51 @@ class _shoppingBasketState extends State<shoppingBasket> {
         return Colors.blue;
       }
       return Color.fromARGB(255, 52, 160, 164);
-    }
-    return Row(
+    }*/
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => setState(() => mapaCompra.addEntries([MapEntry(Ingrediente("${_cont++}"), false)])),
+      ),
+      body: ListView(
+        children: mapaCompra.keys.map(
+              (ingrediente) => CheckboxListTile(
+                title: Text(ingrediente.nombre),
+                secondary: Text(
+                    "#${++_cont}",
+                    textAlign: TextAlign.center,
+                ),
+                controlAffinity: ListTileControlAffinity.trailing,
+                value: mapaCompra[ingrediente],
+                onChanged: (bool? value){
+                  setState(() {
+                    mapaCompra[ingrediente] = value!;
+                  });
+                },
+                activeColor: Color.fromARGB(255, 52, 160, 164),
+                checkColor: Colors.white,
+          ),
+        ).toList(),
+      ),
+    );
+
+    /*return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            for (i = 0; i<l1.length(); i++) ...[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(l1.get(i).toString()),
-                  Checkbox(
-                    checkColor: Colors.white,
-                    fillColor: MaterialStateProperty.resolveWith(getColor),
-                    value: l1.get(i)["check"], //Aqui habria que establecer a value al bool del map
-                    onChanged: (bool? value) {
-                      setState(() {
-                        l1.get(i)["check"] = value!;
-                      });
-                    },
-                  ),
-                ],
-              )
-            ]
-          ],
-        )
-      ],
-    );
+      children: ingredientes.keys.map((ingrediente) {
+        return new CheckboxListTile(
+          checkColor: Colors.white,
+          //fillColor: MaterialStateProperty.resolveWith(getColor),
+          title: new Text(ingrediente.nombre),
+          value: ingredientes[ingrediente],
+          onChanged: (bool? value) {
+            setState(() {
+              ingredientes[ingrediente] = value!;
+            });
+        },
+      );
+    }).toList(),
+    ); */
   }
 }
