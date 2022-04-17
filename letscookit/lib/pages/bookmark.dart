da29d3006e469f166e0f91561c1fca6d71d5d3a7
elB1MBO <raulduran.lacimada@gmail.com>
2022-04-17 16:32:02 +0200

    libro recetas listado

--- letscookit/lib/pages/bookmark.dart
+++ letscookit/lib/pages/bookmark.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:letscookit/config/palette.dart';
+import 'package:letscookit/utilities/libro_recetas.dart';
+import 'package:letscookit/utilities/lista_receta.dart';
+
+import '../utilities/receta.dart';
 
 class Bookmark extends StatefulWidget {
   Bookmark({Key? key}) : super(key: key);
@@ -8,10 +13,59 @@ class Bookmark extends StatefulWidget {
 }
 
 class _BookmarkState extends State<Bookmark> {
+  List<Receta> lista = [];
+  LibroRecetas libroRecetas = LibroRecetas();
+  ListaReceta listaRecetas = ListaReceta("Mi Lista");
+  String _receta = "Receta1"; //Tiene que estar inicializado al valor de alguno de los items
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Bookmarks'),
+    //libroRecetas.add(listaRecetas);
+    return Column(
+      children: [
+        ListView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: libroRecetas.length(),
+            itemBuilder: (BuildContext context, int index){
+              return Card(
+                child: ListTile(
+                  title: Text(libroRecetas.get(index).nombre),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => libroRecetas.get(index),
+                        ),
+                    );
+                  },
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('assets/elfary.jpg'),
+                  ),
+                ),
+              );
+            }
+        ),
+      ],
     );
   }
+
+  //Metodo que redirige al usuario a la receta que ha seleccionado del menu desplegable
+  void viewReceta(String? selectedReceta){ //se le pasa Receta receta
+    if(selectedReceta is String){
+      setState(() {
+        _receta = selectedReceta;
+      });
+    }
+  }
+  /*
+  void viewReceta(Receta? selectedReceta){ //se le pasa Receta receta
+    if(selectedReceta is Receta){
+      setState(() {
+        _receta = selectedReceta;
+      });
+    }
+  }
+  */
 }
+
